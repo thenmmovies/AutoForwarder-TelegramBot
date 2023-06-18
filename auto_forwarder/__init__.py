07af6a03116ce72619975e1f4b7c185b457a954e
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2023-06-18 20:17:41 +0530

    Update __init__.py

--- auto_forwarder/__init__.py
+++ auto_forwarder/__init__.py
@@ -11,35 +11,6 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-
-ENV = bool(os.environ.get('ENV', False))
-
-if ENV:
-    API_KEY = os.environ.get('API_KEY', None)
-    try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    try:
-        FROM_CHATS = set(int(x) for x in os.environ.get("FROM_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your FROM_CHATS list does not contain valid integers.")
-
-    try:
-        TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your TO_CHATS list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    IP_ADDRESS = os.environ.get('IP_ADDRESS', "0.0.0.0")
-    URL = os.environ.get('URL', "")  # Does not contain token
-    PORT = int(os.environ.get('PORT', 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-
-    WORKERS = int(os.environ.get('WORKERS', 4))
-
-else:
     from auto_forwarder.config import Development as Config
     API_KEY = Config.API_KEY
     try:
